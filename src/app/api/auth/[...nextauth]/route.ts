8b82af800526382f802175982e6224957b2db9aa
Jinseop.K <xmcdk@naver.com>
2024-08-25 11:55:17 +0100

    Update the information(secret key)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,11 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string ,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }),
     GitHubProvider({
@@ -14,7 +15,7 @@ const handler = NextAuth({
     }),
   ],
 
-  secret: 'Kukmoda123@', 
+  secret: process.env.NEXTAUTH_SECRET, 
 });
 
 export { handler as GET, handler as POST };
