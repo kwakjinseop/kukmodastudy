ed04fd6a82811d7c49da309c0e3957c478dce784
Jinseop.K <xmcdk@naver.com>
2024-09-04 22:08:32 +0100

    revise by app version

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,16 +1,16 @@
+// src/app/api/cron/route.js
+
+import { NextResponse } from "next/server";
 import fetch from "node-fetch";
 
-// pages/api/send-slack-message.js
-
-export default async function handler(req, res) {
+// POST 요청을 처리하는 함수
+export async function POST(req) {
   const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL; // 환경 변수에서 Webhook URL 읽기
 
-  // 보낼 메시지 데이터
   const messageData = {
     text: "오늘 모각스에 참여하실 분들은 \nzep에 입장해주시고 본 스레드에 오늘의 공부 목표를 답글로 적어주세요.\n\nZEP 링크 : https://zep.us/play/yBdEwz\n\nZEPZEP\n국모다it 메타버스 캠퍼스 | 쉽고 재미있는 메타버스 ZEP\n공부하고 싶다면 누구나 들어오세요~",
   };
 
-  // Slack으로 메시지 전송
   const response = await fetch(slackWebhookUrl, {
     method: "POST",
     headers: {
@@ -19,10 +19,12 @@ export default async function handler(req, res) {
     body: JSON.stringify(messageData),
   });
 
-  // 응답 처리
   if (response.ok) {
-    res.status(200).json({ message: "Message sent to Slack successfully" });
+    return NextResponse.json({ message: "Message sent to Slack successfully" });
   } else {
-    res.status(500).json({ message: "Failed to send message to Slack" });
+    return NextResponse.json(
+      { message: "Failed to send message to Slack" },
+      { status: 500 }
+    );
   }
 }
